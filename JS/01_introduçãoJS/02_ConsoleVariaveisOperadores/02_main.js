febcd49d4e9a839fbd9b278152db540d3dcfc39b
JoelRodrig <joelomar531@gmail.com>
2022-10-10 21:06:18 -0300

    subida de arquivos 12 e 02 js

--- JS/01_introduçãoJS/02_ConsoleVariaveisOperadores/02_main.js
+++ JS/01_introduçãoJS/02_ConsoleVariaveisOperadores/02_main.js
@@ -1,3 +1,23 @@
 document.write('O valor é ' + (299 + 490 + 980)); /*document.write('') serve para alterar no HTML*/
 
 /* \n é igual a tag <br> "porque quebra o texto"*/
+
+var renan = 7;
+var renan2 = 8.5;
+var renan3 = 9.5;
+var renan4 = 10;
+
+console.log('A média do aluno será ' + (renan + renan2 + renan3 + renan4 /4));
+
+/*A média é do aluno será 27.5*/
+
+
+        /*OU
+
+var renan = 7;
+var renan2 = 8.5;
+var renan3 = 9.5;
+var renan4 = 10;
+var media = renan + renan2 + renan3 + renan4 / 4;
+
+console.log('A média do aluno será ' + media);  */
