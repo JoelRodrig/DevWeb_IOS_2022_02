65b529764df7cb06f99a42ee7fdd13da60d1e907
JoelRodrig <joelomar531@gmail.com>
2022-10-10 21:50:07 -0300

    subida de arquivos atualizados

---
+++ JS/01_introduçãoJS/03_Strings/03_main.js
@@ -0,0 +1,14 @@
+var h = 20;
+var j = 30;
+
+var media = h + j / 2;
+console.log(`A média é ${media}`);
+
+
+
+// Acessando um caracter de uma string
+
+var string1 = "Falta a camisa pessoal"
+
+console.log(string1[0]);
+console.log(string1.charAt(3));
