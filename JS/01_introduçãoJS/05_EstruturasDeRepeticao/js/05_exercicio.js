1f70f24215a10e3855dbbf62f7760663eb959a48
JoelRodrig <joelomar531@gmail.com>
2022-10-13 21:42:36 -0300

    subida de exercicios js 05

---
+++ JS/01_introduçãoJS/05_EstruturasDeRepeticao/js/05_exercicio.js
@@ -0,0 +1,23 @@
+// (1) Mostrar no console os números de 1 a 50. (While)
+let contador = 1;
+
+while (contador <= 50) {
+    console.log(`Valor = ${contador}`);
+    contador++;
+}
+
+//(2) Mostrar no console contagem regressiva de 10 até 0 (Do While)
+
+let contagem = 10,
+    regressiva = '';
+
+do {
+    regressiva += ` contagem ${contagem}\n`;
+    contagem--;
+} while (contagem >= 0);
+console.log(regressiva);
+
+//(3) Mostrar no console os números pares de 1 a 100. (For)
+
+for (let i = 1; i <= 10; i++)
+    console.log(` Números pares ${i}`);
