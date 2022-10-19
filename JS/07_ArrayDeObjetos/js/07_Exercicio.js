271b1856349ff8a3d78717ce55cc9ae83a87973c
JoelRodrig <joelomar531@gmail.com>
2022-10-18 21:50:50 -0300

    subida de arquivos 07 js

---
+++ JS/07_ArrayDeObjetos/js/07_Exercicio.js
@@ -0,0 +1,33 @@
+/* Array de objetos */
+
+let alunos = [
+    {
+        nome: 'Mia',
+        notas: [6, 8, 10, 9],
+        sala: '3A',
+        escola: 'EM Cabral junior',
+        endereco: {
+            rua: 'Rua do bobos',
+            numero: 1,
+            cidade: 'Rio de Janeiro',
+            estado: 'RJ',
+        },
+        grau: '2 grau do Ensino Médio',
+        responsável: 'Matheus - Pai',
+        
+    },
+    {
+        nome: 'Thomas',
+        notas: [5, 8, 6, 7],
+        sala: '2B',
+        escola: 'Capitão Amaral',
+        endereco: {
+            rua: 'Rua dos patos',
+            numero: 2,
+            cidade: 'São Paulo',
+            estado: 'SP',
+        },
+        grau: '1 grau do Ensino Médio',
+        responsável: 'Maria - Mãe',
+    }
+]
