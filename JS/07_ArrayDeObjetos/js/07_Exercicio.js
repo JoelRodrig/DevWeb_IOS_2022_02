2afec4225c5913d658313692a6ef2737c6af832c
JoelRodrig <joelomar531@gmail.com>
2022-10-20 16:06:25 -0300

    subida de exercicios js 07

--- JS/07_ArrayDeObjetos/js/07_Exercicio.js
+++ JS/07_ArrayDeObjetos/js/07_Exercicio.js
@@ -3,31 +3,52 @@
 let alunos = [
     {
         nome: 'Mia',
+        idade: 16,
         notas: [6, 8, 10, 9],
         sala: '3A',
         escola: 'EM Cabral junior',
         endereco: {
-            rua: 'Rua do bobos',
-            numero: 1,
-            cidade: 'Rio de Janeiro',
-            estado: 'RJ',
+            rua: 'Rua dos mosqueteiros',
+            numero: 14,
+            cidade: 'São Paulo',
+            estado: 'SP',
         },
-        grau: '2 grau do Ensino Médio',
+        escolaridade: '2 grau do Ensino Médio',
         responsável: 'Matheus - Pai',
-        
+
     },
     {
         nome: 'Thomas',
+        idade: 15,
         notas: [5, 8, 6, 7],
         sala: '2B',
         escola: 'Capitão Amaral',
         endereco: {
             rua: 'Rua dos patos',
-            numero: 2,
+            numero: 28,
             cidade: 'São Paulo',
             estado: 'SP',
         },
-        grau: '1 grau do Ensino Médio',
+        escolaridade: '1 grau do Ensino Médio',
         responsável: 'Maria - Mãe',
+    },
+    {
+        nome: 'Gabi',
+        idade: 17,
+        notas: [5, 8, 6, 7],
+        sala: '2B',
+        escola: 'Capitão Pedroso',
+        endereco: {
+            rua: 'Rua alameda salete',
+            numero: 89,
+            cidade: 'São Paulo',
+            estado: 'SP',
+        },
+        escolaridade: '1 grau do Ensino Médio',
+        responsável: 'Marcos - Pai',
     }
-]
+];
+
+
+
+// console.log(JSON.stringify(alunos));
