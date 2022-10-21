bd183c39d2a47c3a705f930801536ee4f6dba062
JoelRodrig <joelomar531@gmail.com>
2022-10-21 18:15:51 -0300

    subida de exercicios js 08

--- JS/08_ObjetosFuncoesEventos/js/08_Exercicio.js
+++ JS/08_ObjetosFuncoesEventos/js/08_Exercicio.js
@@ -1,8 +1,9 @@
 function calculo(a = 3, b = 6, c = -7) {
-    return b * b -4 * a * c;
+    const delta = b * b -4 * a * c;
+
+const x1 = (-b + Math.sqrt(delta))/(2 * a);
+const x2 = (-b - Math.sqrt(delta))/(2 * a);
+
+console.log(x1);
+console.log(x2);
 }
-const x1 = (-b + Math.sqrt(calculo))/(2 * a);
-const x2 = (-b - Math.sqrt(calculo))/(2 * a);
-
-
-
