5bf6e1a1a13ba6e7134b88b5236b238b5369b1da
JoelRodrig <joelomar531@gmail.com>
2022-10-20 21:56:54 -0300

    subida de arquivos js e imagens tcc

--- JS/07_ArrayDeObjetos/js/07_Exercicio.js
+++ JS/07_ArrayDeObjetos/js/07_Exercicio.js
@@ -3,7 +3,7 @@
 let alunos = [
     {
         nome: 'Mia',
-        idade: 16,
+        idade: 10,
         notas: [6, 8, 10, 9],
         sala: '3A',
         escola: 'EM Cabral junior',
@@ -15,13 +15,12 @@ let alunos = [
         },
         escolaridade: '2 grau do Ensino Médio',
         responsável: 'Matheus - Pai',
-
     },
     {
         nome: 'Thomas',
         idade: 15,
-        notas: [5, 8, 6, 7],
-        sala: '2B',
+        notas: [5, 8, 4, 7],
+        sala: '33',
         escola: 'Capitão Amaral',
         endereco: {
             rua: 'Rua dos patos',
@@ -35,8 +34,8 @@ let alunos = [
     {
         nome: 'Gabi',
         idade: 17,
-        notas: [5, 8, 6, 7],
-        sala: '2B',
+        notas: [5, 8, 8, 7],
+        sala: '54',
         escola: 'Capitão Pedroso',
         endereco: {
             rua: 'Rua alameda salete',
@@ -46,9 +45,23 @@ let alunos = [
         },
         escolaridade: '1 grau do Ensino Médio',
         responsável: 'Marcos - Pai',
-    }
+    },
 ];
 
+let soma = 0;
+let media = 0;
+let aprov = 0;
+
+for (let x = 0; x < alunos.length; x++) {
+    soma = 0;
+    for (let i = 0; i < alunos[x].notas.length; i++) soma += alunos[x].notas[i];
+
+    media = soma / alunos[x].notas.length;
+    console.log(`media ${media}`);
+
+    aprov = media >= 7 ? "aprovado" : "reprovado";
+    console.log(` Aluno ${aprov}`)
+
+}
 
 
-// console.log(JSON.stringify(alunos));
---
+++ JS/08_ObjetosFuncoesEventos/js/08_Exercicio.js
@@ -0,0 +1,8 @@
+function calculo(a = 3, b = 6, c = -7) {
+    return b * b -4 * a * c;
+}
+const x1 = (-b + Math.sqrt(calculo))/(2 * a);
+const x2 = (-b - Math.sqrt(calculo))/(2 * a);
+
+
+
