c3be3eb71bd7cae9c8b1684dc5fbb2e8d75db7b5
JoelRodrig <joelomar531@gmail.com>
2022-10-28 21:25:00 -0300

    subida de arquivos js

--- JS/12_ManipulandoOdomPart2/js/12_Exercicio.js
+++ JS/12_ManipulandoOdomPart2/js/12_Exercicio.js
@@ -1,16 +1,16 @@
-function alterador1() {
+function Alterador1() {
     const item1 = document.getElementById('item1');
     item1.style.backgroundColor = 'aqua';
     console.log(item1);
 }
 
-function alterador2() {
+function Alterador2() {
     let item2 = document.getElementsByClassName('item');
     item2[1].style.fontWeight = 'bold';
     console.log(items[1]);
 }
 
-function alterador3() {
+function Alterador3() {
     let li = document.getElementsByTagName('li');
     console.log(li);
     for (let i = 0; i < li.length; i++) {
@@ -19,7 +19,7 @@ function alterador3() {
     }
 }
 
-function alterador4() {
+function Alterador4() {
     let nome = document.getElementsByName('fitem');
     nome[0].style.borderBottom = 'solid 5px red';
     console.log(nome);
@@ -27,14 +27,14 @@ function alterador4() {
 
 let item5 = document.getElementById('item5');
 
-function alterador5(){
+function Alterador5(){
     item5.remove();
 }
 
 let lista = document.getElementById('items');
 let item4 = document.getElementById('item4');
 
-function alterador6(){
+function Alterador6(){
     lista.insertBefore(item5, item4.nextSibling);
     item5.textContent = 'Item recuperado';
 }
