3df342e21a8382de41d24498cb457a1b96ef3242
JoelRodrig <joelomar531@gmail.com>
2022-11-08 21:42:44 -0300

    subida de arquivo react

---
+++ REACT/exercicio_03_componentes/src/Components/BoasVindas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BoasVindas = () => {
+  return (
+    <section>
+        <h1>BoasVindas</h1>
+        <p></p>
+    </section>
+  )
+}
+
+export default BoasVindas
---
+++ REACT/exercicio_03_componentes/src/Components/Sobre.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sobre = () => {
+  return (
+    <article>
+        <h2></h2>
+        <p></p>
+    </article>
+  )
+}
+
+export default Sobre
