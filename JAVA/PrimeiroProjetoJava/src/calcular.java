a03cc0dde6876d2210cc7f64dd9183e96af675f6
JoelRodrig <joelomar531@gmail.com>
2022-11-21 21:51:01 -0300

    subida de aquivos java

---
+++ JAVA/PrimeiroProjetoJava/src/calcular.java
@@ -0,0 +1,16 @@
+public class calcular {
+    // prmeiro passo é criar o metodo main
+    public static void main(String[] args) {
+        // vamos somar a idade de dois alunos
+        // var idade1 = 25;
+        // var idade2 = 15;
+        // int é igual a var "em números inteiros";
+        // double ou float é igual var "em números quebrados";
+        int idade1 = 25;
+        int idade2 = 15;
+        int somaIdade = idade1 + idade2;
+
+        System.out.println("A soma da idade " +
+                "dos aluos é: " + somaIdade);
+    }
+}
---
+++ JAVA/PrimeiroProjetoJava/src/pipoca.java
@@ -0,0 +1,10 @@
+public class pipoca {
+    // mostrar no console a frase 'hello World'
+    public static void main(String[] args)
+    {
+        // console.log("Hello  World")
+        String texto = "hello World";
+        System.out.println(texto);
+        System.out.println(45);
+    }
+}
