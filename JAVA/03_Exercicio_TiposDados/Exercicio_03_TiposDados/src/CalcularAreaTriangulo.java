c0b7e546ef2e53df17769e35185ac3755efb7f05
JoelRodrig <joelomar531@gmail.com>
2022-11-22 21:26:03 -0300

    subida de arquivos java

---
+++ JAVA/03_Exercicio_TiposDados/Exercicio_03_TiposDados/src/CalcularAreaTriangulo.java
@@ -0,0 +1,9 @@
+public class CalcularAreaTriangulo {
+    public static void main(String[] args) {
+        int base = 20;
+        int altura = 30;
+        int calcularArea = (base + altura) / 2;
+
+        System.out.println("A área do triangulo é: " + calcularArea);
+    }
+}
---
+++ JAVA/03_Exercicio_TiposDados/Exercicio_03_TiposDados/src/CalcularMediaNotas.java
@@ -0,0 +1,11 @@
+public class CalcularMediaNotas {
+    public static void main(String[] args) {
+        int aluno1 = 3;
+        int aluno2 = 5;
+        int aluno3 = 7;
+        int aluno4 = 9;
+
+        int somaNotas = (aluno1 + aluno2 + aluno3 + aluno4) / 4;
+        System.out.println("A média do aluno é: " + somaNotas);
+    }
+}
