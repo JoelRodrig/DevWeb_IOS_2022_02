b71cb15074e71b3f3cc012ad5d5dfbafefe58b0f
JoelRodrig <joelomar531@gmail.com>
2022-11-24 13:59:51 -0300

    subida de arquivos home e sobre nos TCC

--- TRABALHO - TCC/MotorsFastPower/Home/js/app.js
+++ TRABALHO - TCC/MotorsFastPower/Home/js/app.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const headerBtn = document.querySelector('.header .nav-bar .nav-list .header-btn');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
+headerBtn.addEventListener('click', () => {
+	headerBtn.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
@@ -19,7 +19,7 @@ document.addEventListener('scroll', () => {
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
+		headerBtn.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
 });
